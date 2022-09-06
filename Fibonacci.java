2225f73ea239ee11483f2ed08b2705615a749edf
sumitshelar3 <112796527+sumitshelar3@users.noreply.github.com>
2022-09-06 14:43:16 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int firstNumber = 0, secondNumber = 1, sum = 0;
+		System.out.println(firstNumber);
+		System.out.println(secondNumber);
+		for (int i = 3; i <= 11; i++) {
+
+			sum = firstNumber + secondNumber;
+			System.out.println(sum);
+
+			firstNumber = secondNumber;
+			secondNumber = sum;
+
+		}
+
+	}
+
+}
